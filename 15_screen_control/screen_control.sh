9b73726109c864249b23cee6cccf32ff832d3d18
Chris Guest <chris.guest@condenastint.com>
2020-04-11 21:31:33 +0100

    Add movement to the bars

--- 15_screen_control/screen_control.sh
+++ 15_screen_control/screen_control.sh
@@ -10,9 +10,10 @@ function generate_background {
         local lineout=""
         for col in $(seq 0 $2); 
             do 
-                local r=$(( 255 - ( col * 255 / 120 ) ))
-                local g=$(( col * 510 / 120 ))
-                local b=$(( col * 255 / 120 ))
+                local offset=$(( col + line )) 
+                local r=$(( 255 - ( offset * 255 / 120 ) ))
+                local g=$(( offset * 510 / 120 ))
+                local b=$(( offset * 255 / 120 ))
                 if [[ $g -gt 255 ]]; then 
                     g=$(( 510-g ))
                 fi
@@ -46,16 +47,33 @@ clear
 SCREEN_Y=$(( $LINES - 2 ))
 generate_background $SCREEN_Y $(( $COLUMNS - 1 ))
 #exit
+offset=0
+direction=0
 while [[ : ]]; do 
+
     printf "\033[0;0H"
     printf "[$COLUMNS x $LINES]\n"
     
     for line in $(seq 0 $(( $LINES - 2 )) ); 
     do
-        printf "${screenbuf[line]}"
+
+        printf "${screenbuf[offset]}"
     done
 
+    if [[ $direction -eq 0 ]]; then
+        offset=$(( offset + 1 ))
+        if [[ $offset -eq 60 ]]; then
+            direction=1
+        fi 
+    else
+        offset=$(( offset - 1 ))
+        if [[ $offset -eq 0 ]]; then
+            direction=0
+        fi 
+    fi
+
+
     printf "\033[0m"
-    sleep 1
+    #sleep 1
 
 done 
