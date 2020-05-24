bdef7d0913f66a0a1fd5ecb85140c8f187c262a9
Chris Guest <chris.guest@condenastint.com>
2020-05-24 20:00:55 +0100

    Add averaging.

--- 19_timing_operations/time-it.sh
+++ 19_timing_operations/time-it.sh
@@ -55,5 +55,28 @@ for count in $(seq "$ITERATIONS"); do
     time_function $FUNCTION_TO_TEST "$@"
 done
 
-printf '%s\n' "${time_samples[@]}"
+#printf '%s\n' "${time_samples[@]}"
 
+IFS=$'\n' sorted=($(sort -g <<<"${time_samples[*]}")); unset IFS
+printf "%s\n" "${sorted[@]}"
+
+ awk '
+  BEGIN {
+    c = 0;
+    sum = 0;
+  }
+  $1 ~ /^(\-)?[0-9]*(\.[0-9]*)?$/ {
+    a[c++] = $1;
+    sum += $1;
+  }
+  END {
+    ave = sum / c;
+    if( (c % 2) == 1 ) {
+      median = a[ int(c/2) ];
+    } else {
+      median = ( a[c/2] + a[c/2-1] ) / 2;
+    }
+    OFS="";
+    print "SUM:",sum, " COUNT:", c, " AVG:", ave, " MEDIAN:", median, " MIN:", a[0], " MAX:",a[c-1];
+  }
+' <(printf "%s\n" "${sorted[@]}")
