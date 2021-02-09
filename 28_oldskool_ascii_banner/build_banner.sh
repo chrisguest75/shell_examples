c3c6d44cfe9518f477ea4f6b65d06a6657f7f854
Chris Guest <chris.guest75@hotmail.com>
2021-02-09 21:58:27 +0000

    Take txt strings.

--- 28_oldskool_ascii_banner/build_banner.sh
+++ 28_oldskool_ascii_banner/build_banner.sh
@@ -2,12 +2,28 @@
 
 OUT=./letters
 
+ord() {
+  LC_CTYPE=C printf '%d' "'$1"
+}
+
 #67 C
 #85 U
 #68 D
 #68 D
 #76 L
 #89 Y
+text=$1
+textlen=${#text}
+filelist=""
+for index in $(seq 0 $(( textlen - 1)) ); 
+do 
+    letter=${text:$index:1}
+    #echo $letter
+    #echo $(ord $letter)
+    filelist="$filelist ${OUT}/$(ord $letter).jpg"
+done
+#echo $filelist
 
-convert ${OUT}/67.jpg ${OUT}/85.jpg ${OUT}/68.jpg ${OUT}/68.jpg ${OUT}/76.jpg ${OUT}/89.jpg +append banner.jpg
-jp2a --term-width --colors --color-depth=24 --fill ./banner.jpg
+convert $filelist +append banner.jpg
+#jp2a --term-width --colors --color-depth=24 --fill ./banner.jpg
+jp2a --width=$(( textlen * 32 )) --colors --color-depth=24 --fill ./banner.jpg
