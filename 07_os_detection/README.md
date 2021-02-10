ed0c62f7c6d31b7be42c91e7c1b23692013685d6
Chris Guest <chris.guest@condenastint.com>
2021-02-10 10:22:38 +0000

    Example os detect

---
+++ 07_os_detection/README.md
@@ -0,0 +1,6 @@
+# README
+Demonstrate how to detect the OS type in script to change parameters to commands. 
+
+```sh
+./detect.sh  
+```
---
+++ 07_os_detection/detect.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash 
+#Use !/bin/bash -x  for debugging 
+set -euf -o pipefail
+
+readonly SCRIPT_NAME=$(basename "$0")
+readonly SCRIPT_PATH=${0}
+# shellcheck disable=SC2034
+readonly SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+
+OSNAME="$(uname -s)"
+case "${OSNAME}" in
+    Linux*)     readonly OSTYPE=LINUX;;
+    Darwin*)    readonly OSTYPE=MAC;;
+    CYGWIN*)    readonly OSTYPE=CYGWIN;;
+    MINGW*)     readonly OSTYPE=MINGW;;
+    *)          readonly OSTYPE="UNKNOWN:${OSNAME}"
+esac
+
+echo "SCRIPT_NAME: ${SCRIPT_NAME}"
+echo "SCRIPT_PATH: ${SCRIPT_PATH}"
+echo "SCRIPT_DIR: ${SCRIPT_DIR}"
+echo "OS Type is ${OSTYPE}"
+case "${OSTYPE}" in
+    LINUX)     
+        lsb_release -a 
+    ;;
+    MAC)    
+        sw_vers -productName
+        sw_vers -productVersion
+        sw_vers -buildVersion
+    ;;
+    CYGWIN)
+    
+    ;;
+    MINGW)
+    
+    ;;
+    *)
+    ;;
+esac
+
+if [[ ${OSTYPE} == "MAC" ]]; then
+    readonly SYMLINKED_PATH="$(dirname "$(greadlink -f "$0")")"
+else
+    readonly SYMLINKED_PATH="$(dirname "$(readlink -f "$0")")"
+fi
+echo "${SYMLINKED_PATH}"
