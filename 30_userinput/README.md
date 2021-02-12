2cedbf80421a8591a467f6c9207819f5a5a4e464
Chris Guest <chris.guest75@hotmail.com>
2021-02-12 21:32:59 +0000

    Add better use input examples

--- 30_userinput/README.md
+++ 30_userinput/README.md
@@ -2,7 +2,12 @@
 Demonstrate how to use user input in scripts
 
 ## Examples
+```sh
+# Answer questions and use as input
+./questions.sh
+```
 
 ```sh
-questions.sh
+# Have an infinite loop and listen for keys
+./quit_on_keypresses.sh
 ```
---
+++ 30_userinput/quit_on_keypress.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+mode='d'
+echo "Press (d) for date, (t) for timecode or (q) to quit"
+
+while true; do
+
+    # Loop command
+    if [[ "$mode" == 'd' ]];then
+        echo "#################################"
+        echo -n "$(date)"
+        echo ""
+        echo "#################################"
+        echo -ne "\e[3A\r"
+    elif  [[ "$mode" == 't' ]];then
+        echo "#################################"
+        echo -n "$(date +%s)                   "
+        echo ""
+        echo "#################################"
+        echo -ne "\e[3A\r"
+    else
+        echo "#################################"
+        echo -n "Unrecognised mode"
+        echo ""
+        echo "#################################"
+        echo -ne "\e[3A\r"
+    fi
+
+    # Check for 'q' keypress *waiting very briefly*  and exit the loop, if found.
+    read -t 0.01 -r -s -N 1 
+    case "${REPLY}" in
+        'q'|'Q')
+            break
+        ;;
+        'd'|'D')
+            mode='d'
+        ;;
+        't'|'T')
+            mode='t'
+        ;;  
+        *)
+            #mode=${REPLY}
+            #echo "Unrecognised ${REPLY}"; 
+        ;;
+    esac        
+
+done
+
+# Post-loop command
+echo "#################################"
+echo "$(date)                           "
+echo "$(date +%s)                       "
+echo "#################################"
