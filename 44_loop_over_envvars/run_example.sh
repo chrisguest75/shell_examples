73df41f43634b4107d8790661af7163f62975c76
Chris Guest <chris.guest@condenastint.com>
2021-07-06 17:37:45 +0100

    Improve the example by having associated variables.

--- 44_loop_over_envvars/run_example.sh
+++ 44_loop_over_envvars/run_example.sh
@@ -13,8 +13,17 @@ echo "*************************************"
 VARIABLES=$(set | grep -Eo "^MY_URL_[0-9]{1,9}=" | grep -Eo "^MY_URL_[0-9]{1,9}")
 while IFS=' ', read -r VARIABLE
 do
+    NUMBER=$(echo "$VARIABLE" | grep -Eo "[0-9]{1,9}")
+    echo "Variable number is $NUMBER"
     # use ${!x} to print a variable from a string
     echo "$VARIABLE is ${!VARIABLE}"
+
+    # is there an associated retries variable?
+    RETRIESNAME=MY_URL_RETRIES_$NUMBER 
+    if [[ ! -z ${!RETRIESNAME} ]]; then 
+        echo "$RETRIESNAME is ${!RETRIESNAME}"
+    fi
+
 done <<< "$VARIABLES"
 
 echo ""
