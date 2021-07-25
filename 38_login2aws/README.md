0cf31698d28fd4048447dde16ea459fab4a48e8d
Chris Guest <chris.guest@condenastint.com>
2021-07-25 23:13:03 +0100

    Switch to login2aws

---
+++ 38_login2aws/README.md
@@ -0,0 +1,36 @@
+# README
+Demonstrate how to map an input to an output
+
+## Install completions
+```sh
+# source the completion script
+. ./login2aws-completion.bash             
+# or
+source ./login2aws-completion.bash             
+```
+
+## See basic account options
+```sh
+# print options
+./login2aws <tab>
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## Resources 
+[Checking sourcing](https://stackoverflow.com/questions/2683279/how-to-detect-if-a-script-is-being-sourced)  
---
+++ 38_login2aws/login2aws-completion.bash
@@ -0,0 +1,40 @@
+#!/bin/bash -x 
+#!/usr/bin/env bash 
+#Use !/bin/bash -x  for debugging 
+autoload bashcompinit
+bashcompinit
+
+_test_script_completion()
+{
+  if [[ -n $COMPLETION_DEBUG ]]; then
+    # Used for debugging.
+    COMPREPLY=()
+    echo ""
+    echo "$$1 : $1"
+    echo "$$2 : $2"
+    echo "$$3 : $3"
+    echo "COMP_WORDS : ${COMP_WORDS}"
+    echo "COMP_CWORD : ${COMP_CWORD}"
+    echo "COMP_WORDS[COMP_CWORD] : ${COMP_WORDS[COMP_CWORD]}"
+    echo "COMP_WORDS[COMP_CWORD-1] : ${COMP_WORDS[COMP_CWORD-1]}"
+    echo "COMP_LINE : ${COMP_LINE}"
+    echo "COMP_POINT : ${COMP_POINT}"
+    echo "COMP_KEY : ${COMP_KEY}"
+    echo "COMP_TYPE : ${COMP_TYPE}"
+    echo "args : $@"
+    echo "reply : ${COMPREPLY}"
+  else
+    # COMP_CWORD is the current work completion index
+    case ${COMP_CWORD} in
+      1)
+        COMPREPLY=($(compgen -W "$(./login2aws --autocomplete)"))
+      ;;    
+      *)
+        COMPREPLY=()
+      ;;
+    esac
+
+  fi
+}
+
+complete -F _test_script_completion login2aws
