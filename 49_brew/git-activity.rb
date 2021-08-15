b80e2f595f04afffbf44e07fecaeecf392dbde4f
Chris Guest <chris.guest@trint.com>
2021-08-15 23:11:42 +0100

    First attempts for homebrew.

--- 49_brew/git-activity.rb
+++ 49_brew/git-activity.rb
@@ -1,16 +1,16 @@
 class GitActivity < Formula
-  desc "Git activity report on branches"
-  homepage ""
-  url "https://github.com/chrisguest75/shell_examples/releases/download/0.0.1-865a1fd/git-activity-release.tar.gz"
-  sha256 "3269ef7af68ab0f6570dc12f9579bc36f9217b9f76c44ee0a4315a1e03d97baf"
+  desc "Generate a report on git repos and PR branches"
+  homepage "https://github.com/chrisguest75/shell_examples"
+  url "https://github.com/chrisguest75/shell_examples/releases/download/0.0.1-f43376d/git-activity-release.tar.gz"
+  sha256 "1a97b6ac991f8f2547c7807603663373223c7faad79239031af29427a398816b"
   license "MIT"
 
-  #bottle do
-  #  sha256 cellar: :any_skip_relocation, all: "66c940e56f22cd2f5fdb3cfd2e1ddd741400c0739bb91ded95f5ddf5fa3c9902"
-  #end
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "66c940e56f22cd2f5fdb3cfd2e1ddd741400c0739bb91ded95f5ddf5fa3c9902"
+  end
 
-  depends_on "spark" 
-  depends_on "gh" 
+  depends_on "gh"
+  depends_on "spark"
 
   def install
     bin.install "git-activity"
