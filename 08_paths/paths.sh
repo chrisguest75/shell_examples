ffed61871d67fc61d137774cd2f9230c2386b6da
Chris Guest <chris.guest75@hotmail.com>
2021-10-17 17:16:01 +0100

    Add extension and filename parsing example to paths.sh

--- 08_paths/paths.sh
+++ 08_paths/paths.sh
@@ -18,6 +18,9 @@ readonly SCRIPT_NAME=$(basename "$0")
 readonly SCRIPT_PATH=${0}
 readonly SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 readonly HOME_DIR=~
+readonly EXTENSION="${SCRIPT_NAME##*.}"
+readonly FILENAME="${SCRIPT_NAME%.*}"
+
 if [[ $(command -v greadlink) ]]; then 
     # mac requires 'brew install coreutils'
     readonly SCRIPT_FULL_PATH="$(dirname "$(greadlink -f "$0")")/${SCRIPT_NAME}"
@@ -30,7 +33,9 @@ echo "SCRIPT_NAME=${SCRIPT_NAME}"
 echo "SCRIPT_PATH=${SCRIPT_PATH}"
 echo "SCRIPT_DIR=${SCRIPT_DIR}"
 echo "HOME_DIR=${HOME_DIR}"
-echo "SCRIPT_FULL_PATH=${SCRIPT_FULL_PATH}"
+echo "SCRIPT_DIR=${SCRIPT_DIR}"
+echo "EXTENSION=${EXTENSION}"
+echo "FILENAME=${FILENAME}"
 
 readonly PARAMETER1=${1-}
 echo "PARAMETER1=${PARAMETER1} LENGTH=${#PARAMETER1}"
